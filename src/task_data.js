5e1e15a775a60726d448f97454019adc1145702d
Jason Whisnant <tasups@gmail.com>
2022-03-24 21:37:31 +0000

    added some data to task_data.js

--- src/task_data.js
+++ src/task_data.js
@@ -3,7 +3,7 @@ const DATA = [
     id: 1,
     key: 1,
     title: "Complete this App",
-    desc: "this is where the information of what it is you're doing goes",
+    desc: "figure out what to do next and put it in this app",
     completed: false
   },
   {
@@ -15,58 +15,58 @@ const DATA = [
   },
   {
     id: 3,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
+    key: 3,
+    title: "Prep for Roleplay Friday",
+    desc: "PCs review, Likely Scenes review, map for vtt, make sure to have monsters and treasure ready",
     completed: false
   },
   {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
+    id: 4,
+    key: 4,
+    title: "Network",
+    desc: "Connect with someone at Beck Digital",
     completed: false
   },
   {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
+    id: 5,
+    key: 5,
+    title: "Learn Javascript",
+    desc: "learn array methods, control flow, and other stuff enough to do shit",
+    completed: true
+  },
+  {
+    id: 6,
+    key: 6,
+    title: "Learn HTML5",
+    desc: "be able to pass a test on it",
+    completed: true
+  },
+  {
+    id: 7,
+    key: 7,
+    title: "Learn enough CSS3",
+    desc: "be able to functionally design something by yourself and not destroy the machine you're on",
+    completed: true
+  },
+  {
+    id: 8,
+    key: 8,
+    title: "Buy something for yourself",
+    desc: "probably a book on coding or rpgs",
     completed: false
   },
   {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
+    id: 9,
+    key: 9,
+    title: "Call you mom and dad",
+    desc: "do this about once every two weeks",
     completed: false
   },
   {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
-    completed: false
-  },
-  {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
-    completed: false
-  },
-  {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
-    completed: false
-  },
-  {
-    id: 1,
-    key: 1,
-    title: "Complete this step",
-    desc: "this is where the information of what it is you're doing goes",
+    id: 10,
+    key: 10,
+    title: "Love your family",
+    desc: "Spend time with your family, kids and wife mainly. Kiss the kidies. Play games with them. Fuck Anna at least a few times a month.",
     completed: false
   }
 ];
